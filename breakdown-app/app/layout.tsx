6b4fda826ed8058d6272681b04e0ecfddf4abe72
Brooke Bergin <brookebergin27@gmail.com>
2026-05-02 00:42:19 +0000

    downgraded react to match stack better (deprecated???)

--- breakdown-app/app/layout.tsx
+++ breakdown-app/app/layout.tsx
@@ -1,18 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+// import { Geist, Geist_Mono } from "next/font/google";
+// import "./globals.css";
+import { Inter, JetBrains_Mono } from "next/font/google";
 import NavBar from "./components/NavBar";
 import { AuthProvider } from "./lib/auth-context";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const inter = Inter({ subsets: ["latin"] });
+const mono = JetBrains_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Breakdown Ling",
@@ -27,7 +31,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${inter.className} ${mono.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <AuthProvider>
